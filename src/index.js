4c6e40bb25ad5ed3b000b13c3ffb65c25ba6cc6e
Anna Wilson <ajoyna01@gmail.com>
2021-11-11 11:51:04 -0600

    incomplete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('%c HI', 'color: firebrick')
+//const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+function fetchDogs() {
+    return fetch('https://dog.ceo/api/breeds/image/random/4')
+      .then(response => response.json())
+      .then(dogsjson => renderDogPics(json));
+
+      const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+  
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+    }
+ 
+function renderDogPics(dogsjson) {
+    document.querySelector('div#json ')
+}
+        
